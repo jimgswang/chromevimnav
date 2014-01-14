6ed9e58d19473d719bc8c65db2d1eb3435f4d332
jim <jim@Watson.(none)>
2014-01-13 22:29:34 -0800

    add background.test.js

---
+++ spec/background.test.js
@@ -0,0 +1,8 @@
+describe('background.js', function() {
+
+    describe('tabNext()', function() {
+        it('should call update on tab with one higher index', function() {
+
+        });
+    });
+});
